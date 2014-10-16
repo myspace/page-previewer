71a59bdff50ccd92d3c62f668cf6e47911b352f1
Islam Hassan <iah.vector@gmail.com>
2014-10-16 03:50:15 +0200

    added test for Decoding page according to charset header

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var preview = require("./index.js"),
 	assert = require("assert");
 
-var input = [ "http://www.cnn.com", "http://www.google.com", "http://www.yahoo.com", "https://new.myspace.com", "http://www.huffingtonpost.com/2012/11/15/israel-troops-gaza_n_2139151.html", "http://www.huffingtonpost.com/2012/11/15/petraeus-investigation_n_2140268.html", "http://www.huffingtonpost.com/2012/11/15/bp-criminal-settlement-gulf-spill_n_2140162.html", "http://health.yahoo.net/articles/nutrition/photos/12-fish-stay-away#0", "http://www.bbc.co.uk/news/world-middle-east-20349280", "http://www.bbc.co.uk/sport/0/football/20341219", "https://httpsnow.org/", "http://www.hollywoodreporter.com/sites/default/files/2012/10/penguin_books_random_house_a_l.jpg", "http://www.cricinfo.com", "http://www.satheeshnatesan.com", "http://www.theatlanticwire.com/entertainment/2012/12/new-music-genres-2012/59680/", "http://www.vimeo.com/33883554#", "http://www.youtube.com/watch?v=W_H_RS4M0jk" ]
+var input = [ "http://www.cnn.com", "http://www.google.com", "http://www.yahoo.com", "https://new.myspace.com", "http://www.huffingtonpost.com/2012/11/15/israel-troops-gaza_n_2139151.html", "http://www.huffingtonpost.com/2012/11/15/petraeus-investigation_n_2140268.html", "http://www.huffingtonpost.com/2012/11/15/bp-criminal-settlement-gulf-spill_n_2140162.html", "http://health.yahoo.net/articles/nutrition/photos/12-fish-stay-away#0", "http://www.bbc.co.uk/news/world-middle-east-20349280", "http://www.bbc.co.uk/sport/0/football/20341219", "https://httpsnow.org/", "http://www.hollywoodreporter.com/sites/default/files/2012/10/penguin_books_random_house_a_l.jpg", "http://www.cricinfo.com", "http://www.satheeshnatesan.com", "http://www.theatlanticwire.com/entertainment/2012/12/new-music-genres-2012/59680/", "http://www.vimeo.com/33883554#", "http://www.youtube.com/watch?v=W_H_RS4M0jk", "http://av.watch.impress.co.jp/docs/news/20141016_671560.html"]
 
 
 input.forEach(function(val) {
@@ -32,6 +32,8 @@ input.forEach(function(val) {
 				case "http://www.hollywoodreporter.com/sites/default/files/2012/10/penguin_books_random_house_a_l.jpg":
 					assert.ok(!data.loadFailed && (data.contentType == "image/jpeg"), "Loading of an image url failed. -- " + val);
 					break;
+				case "http://av.watch.impress.co.jp/docs/news/20141016_671560.html":
+					assert.ok(data.title == "Android 5.0搭載で4K/30p+光学手ブレ補正の「Nexus 6」 - AV Watch", "Decoding page according to charset header. -- " + val);
 				default:
 					assert.ok(!data.loadFailed, "Test failed. -- " + val);
 			}
